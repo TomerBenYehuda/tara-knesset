191ce712f7df8d9fffe847b80499a508a8cdb973
Eyal Sberro <61418885+Eyalsberro@users.noreply.github.com>
2022-05-11 19:41:24 +0300

    header responsive

--- src/compoents/Headers.jsx
+++ src/compoents/Headers.jsx
@@ -1,13 +1,34 @@
-import { LogoDev } from '@mui/icons-material'
 import React from 'react'
 import { Form, FormControl } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 
 export default function Headers({ searchValue, setSearchValue }) {
 
   const navigate = useNavigate()
 
+  const [state, setState] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    setState(open);
+  };
+
+  const theme = createTheme({
+    components: {
+      MuiDrawer: {
+        styleOverrides: {
+          paper: {
+            backgroundColor: '#184885',
+            color: "white",
+          },
+        },
+      },
+    },
+  });
 
 
 
@@ -34,6 +55,25 @@ export default function Headers({ searchValue, setSearchValue }) {
         <h5 onClick={() => goTo("contactus")}>צור קשר</h5>
         <img className='logojo' src="https://i.ibb.co/HPpJqPp/logo.png" alt="logo" onClick={() => goTo("")} />
         <img className='logotara' src="https://i.ibb.co/KcZgHKm/1.png" alt="logo" onClick={() => goTo("")} />
+      </div>
+
+      <div className='formobileonly'>
+        <React.Fragment>
+
+          <Button onClick={toggleDrawer(true)}><MenuIcon /></Button>
+          <ThemeProvider theme={theme}>
+
+            <Drawer
+              anchor={"right"}
+              open={state}
+              onClose={toggleDrawer(false)}
+            >
+              <img className='logojo' src="https://i.ibb.co/HPpJqPp/logo.png" alt="logo" onClick={() => goTo("")} />
+              <img className='logotara' src="https://i.ibb.co/KcZgHKm/1.png" alt="logo" onClick={() => goTo("")} />
+              <h5 onClick={() => goTo("contactus")}>צור קשר</h5>
+            </Drawer>
+          </ThemeProvider>
+        </React.Fragment>
       </div>
     </div>
   )
