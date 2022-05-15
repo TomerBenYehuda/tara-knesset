0bb06c758dde4bca06527747e8e648dc892aa290
Eyal Sberro <61418885+Eyalsberro@users.noreply.github.com>
2022-05-15 10:41:05 +0300

    logo

--- src/compoents/Headers.jsx
+++ src/compoents/Headers.jsx
@@ -67,9 +67,9 @@ export default function Headers({ searchValue, setSearchValue }) {
               anchor={"right"}
               open={state}
               onClose={toggleDrawer(false)}
-            >
+            > 
               <img className='logojo' src="https://i.ibb.co/9TLp7sj/logo-1.png" alt="logo" onClick={() => goTo("")} />
-              <img className='logotara' src="https://i.ibb.co/KcZgHKm/1.png" alt="logo" onClick={() => goTo("")} />
+              <img className='logotara' src="https://i.ibb.co/KcZgHKm/1.png" alt="logo-tara" onClick={() => goTo("")} />
               <h5 onClick={() => goTo("contactus")}>צור קשר</h5>
             </Drawer>
           </ThemeProvider>
