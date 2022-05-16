c326deebfac5262f2e884cd6a9ded5a59972186b
Eyal Sberro <61418885+Eyalsberro@users.noreply.github.com>
2022-05-16 19:49:48 +0300

    padi

--- src/compoents/Paginations.jsx
+++ src/compoents/Paginations.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Paginations({ postsPerPage, totalPosts, paginate,currentPage }) {
+export default function Paginations({ postsPerPage, totalPosts, paginate, currentPage }) {
 
     const pageNumbers = [];
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -10,6 +10,9 @@ export default function Paginations({ postsPerPage, totalPosts, paginate,current
     return (
         <nav aria-label="Page navigation example">
             <ul className='pagination justify-content-center'>
+                <li className="page-item">
+                    <a className="page-link" onClick={() => paginate(currentPage <= 1 ? currentPage == 1 : currentPage - 1)} href="#!">הקודם</a>
+                </li>
                 {pageNumbers.map(number => (
                     <li key={number} className='page-item'>
                         <a onClick={() => paginate(number)} href='#!' className='page-link'>
@@ -18,7 +21,7 @@ export default function Paginations({ postsPerPage, totalPosts, paginate,current
                     </li>
                 ))}
                 <li className="page-item">
-                    <a className="page-link" onClick={() => paginate(currentPage+1)} href="#!">Next</a>
+                    <a className="page-link" onClick={() => paginate(currentPage + 1)} href="#!">הבא</a>
                 </li>
             </ul>
         </nav>
