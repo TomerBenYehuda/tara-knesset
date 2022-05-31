108298b1048da1490deb598b152fd632b9494dde
Eyal Sberro <61418885+Eyalsberro@users.noreply.github.com>
2022-05-31 10:30:53 +0300

    download excel

--- src/compoents/AdminAddExcel.jsx
+++ src/compoents/AdminAddExcel.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function AdminAddExcel() {
     return (
-        <div style={{"paddingTop": 150}}>
-            <h1>hay</h1>
+        <div className='exceladmin'>
+            <h1>להוספת רשימה נא לבחור קובץ</h1>
+
+            <p>להורדה קובץ לדוגמא הנה לחץ  <a href='https://keneset-api.herokuapp.com/admin/downloadExcel'>הורד</a></p>
+            <br />
             <form action="https://keneset-api.herokuapp.com/admin/uploadfile" enctype="multipart/form-data" method="post">
                 <input type="file" name="uploadfile" accept='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel' />
                 <input type="submit" value="Upload Excel" />
