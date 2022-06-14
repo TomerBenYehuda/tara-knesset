8c65c77218104ca2a8df777236f32df73984f079
Eyal Sberro <61418885+Eyalsberro@users.noreply.github.com>
2022-06-14 17:48:08 +0300

    fitrssypage

--- src/compoents/FirstPage.jsx
+++ src/compoents/FirstPage.jsx
@@ -8,7 +8,7 @@ export default function FirstPage() {
         <section id="kenestheader">
             <div className="container-fluid">
                 <div className='kenestimg'>
-                    <h1 className='centertext'>ספר הטלפונים הדיגיטלי של חברי הכנסת ועוזריהם</h1>
+                    <h1 className='centertext'>ספר הטלפונים הדיגיטלי של חברי הכנסת והממשלה</h1>
                     <a href="#memeberlist" >
                         <ArrowCircleDownOutlinedIcon className='icondown' sx={{ color: "#184885", fontSize: 58 }} />
                     </a>
